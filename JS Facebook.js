8f1a731fd8596348e3b0bdb647f2c03af89c5493
zaiv <capsindo5@gmail.com>
2017-05-07 00:08:39 +0700

    Add files via upload

---
+++ JS Facebook.js
@@ -0,0 +1,9 @@
+<div id='fb-root'/>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = &quot;//connect.facebook.net/id_ID/sdk.js#xfbml=1&amp;version=v2.7&quot;;
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, &#39;script&#39;, &#39;facebook-jssdk&#39;));
+</script>
---
+++ Recent Post Widget.js
@@ -0,0 +1,15 @@
+<style scoped='' type='text/css'>
+ul.recent_posts_arlina li{display:inline-block;border:0;margin:5px 0;padding:5px 0;position:relative;width:100%}
+.recent_posts_arlina .wrapinfo{display:inline-block;margin:0 10px 0 0;overflow:hidden;z-index:2;position:relative;width:60px;height:60px;float:left}
+ul.recent_posts_arlina li img.recent_thumb{width:100%;height:auto}
+.recent_posts_arlina .wrapinfo:before{content:"";z-index:1;height:100%;width:100%;background:rgba(0,0,0,0.2);opacity:0;position:absolute;top:0;left:0;border-radius:50%;transform:scale(0,0);transition:all .4s}
+.recent_posts_arlina .wrapinfo:hover:before{opacity:1;transform:scale(1.5,1.5)}
+.recent_posts_arlina {float:left;width:100%;margin:10px 10px 5px 0px;padding:0;font-size:13px;}
+.recent_posts_arlina strong {font-size:13px;}
+ul.recent_posts_arlina li a{font-weight:400;line-height:normal}
+</style>
+<script style='text/javascript' src='https://rawgit.com/Arlina-Design/FlamingTree/master/recentpostarlina.js'></script>
+<script style='text/javascript'>
+var numposts=5,showpostthumbnails=!0,displaymore=!1,displayseparator=!1,showcommentnum=!1,showpostdate=!1,showpostsummary=!1,numchars=0;
+</script>
+<script src='/feeds/posts/default?orderby=published&alt=json-in-script&callback=recentpostinfoarlina'></script>
